756d00a462cdd7735859ebd29911b123ca22d27b
Axel Delannay <axel.d@courseur.com>
2021-10-01 14:58:22 +0200

    change uri

--- lib/treezor_client/api_client.rb
+++ lib/treezor_client/api_client.rb
@@ -264,7 +264,8 @@ module TreezorClient
     def build_request_url(path)
       # Add leading and trailing slashes to path
       path = "/#{path}".gsub(/\/+/, '/')
-      ERB::Util.url_encode(@config.base_url + path)
+      @config.base_url + path
+      # ERB::Util.url_encode(@config.base_url + path)
       # URI.encode(@config.base_url + path)
     end
 
